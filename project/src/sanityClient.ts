9fabcdb797ada6a6c13ce5201e470f9e396fc468
The RD Group Of Industries <therdgroup24@gmail.com>
2025-08-12 21:03:00 +0530

    id update

--- project/src/sanityClient.ts
+++ project/src/sanityClient.ts
@@ -8,14 +8,14 @@ const token =
   "skno9HTs1HgG3QUDd2hyX0KVKucYSJyDx1s7s4727CR2mnff8xhNGlZHJk0DeJGqQG1zptmrFUjckUXjX0X0u4MYv7w5BOzVGBDUerw81qMmihE0e3NGTlh2pjNUUlKDSov6YCMmbR6csALc00oOdBQGcYtlWgWG2m8EaFNz7psAwhVr0CA8";
 
 export const client: SanityClient = createClient({
-  projectId: "bgi7wgiz",
+  projectId: "bg45uhs5",
   dataset: "production",
   useCdn: true,
   apiVersion: "2023-07-17",
 });
 
 export const secureClient: SanityClient = createClient({
-  projectId: "bgi7wgiz",
+  projectId: "bg45uhs5",
   dataset: "production",
   token,
   useCdn: false,
